f52ca19f7570c9714ed3c92ffe7e2a5baad8f764
Ryan Alam <115599482+rtalam1@users.noreply.github.com>
2023-03-17 18:30:22 -0500

    Functioning grid

--- scripts.js
+++ scripts.js
@@ -1,8 +1,30 @@
 const container = document.querySelector('#container')
-let div1 = document.createElement('div')
-div1.style.backgroundColor = 'blue'
-div1.style.color = 'blue'
-let content = document.createElement('p')
-content.textContent = "Hello"
-container.appendChild(div1)
-div1.appendChild(content)
+
+function genDivs(n) {
+    let e = container
+    for (let i =0;i < n; i++){
+        let row = document.createElement('div')
+        row.className ='row'
+        for(let x = 1; x<= n; x++){
+            let cell = document.createElement('div')
+            cell.className = "gridsquare";
+            //cell.innerText = (i * n) + x;
+            row.appendChild(cell);
+        }
+        e.appendChild(row);
+    }
+}
+genDivs(16)
+
+
+
+
+
+
+
+
+
+
+
+
+
