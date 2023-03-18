0303bb071b937b099f444cd671dfa79bd6c544ae
Ryan Alam <115599482+rtalam1@users.noreply.github.com>
2023-03-17 19:49:01 -0500

    finished hover

--- scripts.js
+++ scripts.js
@@ -8,18 +8,30 @@ function genDivs(n) {
         for(let x = 1; x<= n; x++){
             let cell = document.createElement('div')
             cell.className = "gridsquare";
-            //cell.innerText = (i * n) + x;
+            cell.id = (i * n) + x;
             row.appendChild(cell);
         }
         e.appendChild(row);
     }
 }
+function changeDivColor(x){
+     return x.style.backgroundColor = "blue"
+}
 genDivs(16)
 
+const divs = document.querySelectorAll('.gridsquare')
+console.log(divs)
 
+/*divs.forEach(x => x.addEventListener('mouseenter', function (){
+    divs.forEach(e =>{
+        //let help = e.querySelector('.gridsquare');
+        x.style.backgroundColor = "blue"
+        x.classList.add('test')
 
-
-
+    })
+}))
+*/
+divs.forEach(x => x.addEventListener('mouseenter', () => x.style.backgroundColor ='blue'))
 
 
 
